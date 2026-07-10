e26d286c5c1787ee70bb1f9189b0326048c0c587
JayDee-Tet <jaydeeveea@gmail.com>
2026-07-10 13:25:51 +0800

    Add files via upload

---
+++ movin.js
@@ -0,0 +1,55 @@
+"use strict";
+const navToggle1 = document.getElementById('navToggle1');
+const navLinks = document.getElementById('navLinks');
+const navToggle2 = document.getElementById('navToggle2');
+const navToggle2Links = document.getElementById('navToggle2Links');
+const navCaret = document.getElementById('navCaret');
+const logo = document.getElementById('logoScrollTop');
+
+logo.addEventListener('click', (e) => {
+  e.preventDefault(); 
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+logo.addEventListener('click', (e) => {
+  e.preventDefault(); // stops the "#" from jumping the page
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+navToggle1.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    navToggle1.classList.toggle('active'); //
+});
+
+navToggle2.addEventListener('click', (e) => {
+    e.stopPropagation();
+    navToggle2Links.classList.toggle('active');
+    navCaret.classList.toggle('fa-caret-down');
+    navCaret.classList.toggle('fa-caret-up');
+});
+
+document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (!navLinks.contains(target) && !navToggle1.contains(target)) {
+        navLinks.classList.remove('active');
+        navToggle2Links.classList.remove('active');
+        navToggle1.classList.remove('active');
+        navCaret.classList.remove('fa-caret-up');
+        navCaret.classList.add('fa-caret-down');
+    }
+});
+
+document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (!navLinks.contains(target) && !navToggle1.contains(target)) {
+        navLinks.classList.remove('active');
+        navToggle2Links.classList.remove('active');
+        navToggle1.classList.remove('active');
+    }
+});
